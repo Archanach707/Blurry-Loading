89e5a8317b43477199a6075edc7f9e9e847c1b27
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-06-26 22:09:21 +0530

    Complete js

--- js/index.js
+++ js/index.js
@@ -8,7 +8,19 @@ let int = setInterval(blurring, 30)
 function blurring() {
   load++
 
-  console.log(load)
+  if(load > 90){
+    clearInterval(int)
+  }
+
+  loadText.innerHTML = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+} 
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
 }
+
+
 
 
